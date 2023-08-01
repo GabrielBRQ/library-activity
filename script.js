0e8c0f50bcc78473321dea0bffd3d15e51a62c2f
GabrielBRQ <gabrielmedvalle@gmail.com>
2023-08-01 15:46:03 -0300

    addresponsive design

--- script.js
+++ script.js
@@ -32,7 +32,7 @@ closeButton.addEventListener('click', closeBookForm);
 // Function to handle form submission
 function handleFormSubmit(event) {
   event.preventDefault();
-
+  
   // Get the form input values
   const form = event.target;
   const title = form.elements['book-name'].value;
@@ -57,16 +57,48 @@ function handleFormSubmit(event) {
 //function to update invetory counter
 function updateBookCount() {
   const bookInventoryElement = document.querySelector(".book-inventory p:last-child");
-  const currentCount = parseInt(bookInventoryElement.textContent);
-  const newCount = currentCount + 1;
-  bookInventoryElement.textContent = newCount;
+  const currentInventoryCount = parseInt(bookInventoryElement.textContent);
+  const newInventoryCount = myLibrary.length;
+  bookInventoryElement.textContent = newInventoryCount;
+}
+
+//function to update read counter
+function updateReadCount(read, created) {
+  const bookReadElement = document.querySelector(".book-count p:last-child");
+  const currentReadCount = parseInt(bookReadElement.textContent);
+  
+  if(read == 'read'){
+    const newReadCount = currentReadCount + 1;
+    bookReadElement.textContent = newReadCount;
+  }else if(currentReadCount > 0){
+    if(created){
+      return;
+    }
+    const newReadCount = currentReadCount - 1;
+    bookReadElement.textContent = newReadCount;
+  }
+}
+
+function removeBookFromLibrary(index) {
+  myLibrary.splice(index, 1);
+}
+
+function handleDeleteButtonClick(event) {
+  const bookCard = event.target.closest('.book-card');
+  const bookIndex = Array.from(bookCard.parentElement.children).indexOf(bookCard);
+
+  if (bookIndex !== -1) {
+    removeBookFromLibrary(bookIndex);
+    bookCard.remove();
+    updateBookCount();
+    updateReadCount(myLibrary[bookIndex]?.read, false);
+  }
 }
 
 // Function to create a new div to display book information
 function createBookCard(book) {
   const bookCard = document.createElement('div');
   bookCard.classList.add('book-card');
-  
 
   const bookTitle = document.createElement('h1');
   bookTitle.textContent = `${book.title}`;
@@ -77,17 +109,31 @@ function createBookCard(book) {
   bookCard.appendChild(bookInfo);
 
   const bookButton = document.createElement('button');
-  bookButton.textContent = book.read == 'read' ? 'read' : 'not read';
+  bookButton.textContent = book.read === 'read' ? 'read' : 'not read';
   bookCard.appendChild(bookButton);
+
+  const deleteButton = document.createElement('button');
+  deleteButton.textContent = 'delete';
+  bookCard.appendChild(deleteButton);
+
+  deleteButton.addEventListener('click', handleDeleteButtonClick);
+
+  if (book.read === 'read') {
+    bookButton.style.backgroundColor = '#71aca6';
+  } else {
+    bookButton.style.backgroundColor = '#D05557';
+  }
 
   bookButton.addEventListener('click', () => {
     if (book.read === 'read') {
       book.read = 'not read';
       bookButton.textContent = 'not read';
+      updateReadCount(book.read, false);
       bookButton.style.backgroundColor = '#D05557';
     } else {
       book.read = 'read';
       bookButton.textContent = 'read';
+      updateReadCount(book.read, false);
       bookButton.style.backgroundColor = '#71aca6';
     }
   });
@@ -96,6 +142,8 @@ function createBookCard(book) {
   const bookShelf = document.querySelector('.book-shelf');
   bookShelf.appendChild(bookCard);
 
+
+  updateReadCount(book.read, true);
   updateBookCount();
 }
 
